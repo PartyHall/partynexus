efc7b0a17436f2c18c6c4bd6cd13acf3633fc9eb
Nathan JANCZEWSKI <nathan@janczewski.fr>
2024-11-12 17:03:22 +0100

    fix: Adding some logs

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@
 
 USER := $(shell id -u):$(shell id -g)
 
-VERSION = 0.1.7
+VERSION = 0.1.8
 COMMIT = $(shell git rev-parse --short HEAD)
 
 up:
--- src/State/Provider/SongDownloadProvider.php
+++ src/State/Provider/SongDownloadProvider.php
@@ -4,11 +4,9 @@ namespace App\State\Provider;
 
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProviderInterface;
-use App\Entity\Event;
 use App\Entity\Song;
-use App\Entity\User;
-use App\Repository\EventRepository;
 use App\Repository\SongRepository;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
@@ -21,6 +19,7 @@ use Symfony\Component\HttpKernel\Exception\HttpException;
 readonly class SongDownloadProvider implements ProviderInterface
 {
     public function __construct(
+        private LoggerInterface $logger,
         private SongRepository $repo,
         private Security $security,
         #[Autowire(env: 'SONG_LOCATION')]
@@ -35,6 +34,7 @@ readonly class SongDownloadProvider implements ProviderInterface
         /** @var Song|null $song */
         $song = $this->repo->find($id);
         if (!$song) {
+            $this->logger->error('Download song: Failed to find song in database');
             throw HttpException::fromStatusCode(Response::HTTP_NOT_FOUND);
         }
 
@@ -51,6 +51,7 @@ readonly class SongDownloadProvider implements ProviderInterface
         $songPhk = join('/', [$this->compiledLocation, \sprintf('%s.phk', $song->getId())]);
 
         if (!file_exists($songPhk)) {
+            $this->logger->error('Download song: The file does not exists '.$songPhk);
             throw HttpException::fromStatusCode(Response::HTTP_NOT_FOUND);
         }
 
